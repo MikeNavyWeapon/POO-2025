cff5f72e6ca4460a5f8f6cbe7ef6c3eb27bb0bbc
Montes.Santos <montes.santos@marinha.pt>
2025-10-01 12:18:19 +0100

    Progresso do Lab 02

--- create_structure.bat
+++ create_structure.bat
@@ -1,24 +1,24 @@
 @echo off
-echo Criando estrutura de diretorios para lab01...
+echo Criando estrutura de diretorios para lab02...
 
-mkdir lab01
-mkdir lab01\.vscode
-mkdir lab01\src
-mkdir lab01\src\main
-mkdir lab01\src\main\java
-mkdir lab01\src\main\java\escnaval
-mkdir lab01\src\main\java\escnaval\exemplo
-mkdir lab01\src\main\resources
-mkdir lab01\src\test
-mkdir lab01\src\test\java
-mkdir lab01\out
-mkdir lab01\docs
+mkdir lab02
+mkdir lab02\.vscode
+mkdir lab02\src
+mkdir lab02\src\main
+mkdir lab02\src\main\java
+mkdir lab02\src\main\java\pt
+mkdir lab02\src\main\java\pt\escnaval
+mkdir lab02\src\main\java\pt\escnaval\exercicios
+mkdir lab02\out
 
-echo. > lab01\README.md
-echo. > lab01\.gitignore
-echo. > lab01\.vscode\launch.json
-echo. > lab01\.vscode\tasks.json
-echo. > lab01\src\main\java\escnaval\exemplo\HelloWorld.java
+
+echo. > lab02\src\main\java\pt\escnaval\exercicios\LeituraRobusta.java
+echo. > lab02\src\main\java\pt\escnaval\exercicios\CalcCLI.java
+echo. > lab02\src\main\java\pt\escnaval\exercicios\ConversorUnidades.java
+echo. > lab02\src\main\java\pt\escnaval\exercicios\EstatisticasSimples.java
+echo. > lab02\src\main\java\pt\escnaval\exercicios\UtilsIO.java
+echo. > lab02\.gitignore
+
 
 echo Estrutura criada com sucesso!
 pause
